5524f1096dab3bda63a7469613836e32b88fa6ba
raopremrao <raom61337@gmail.com>
2025-11-01 11:33:29 +0530

    Finally Completed

--- Section-2/foundry-fund-me-f25/src/PriceConverter.sol
+++ Section-2/foundry-fund-me-f25/src/PriceConverter.sol
@@ -1,13 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+// import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "../lib/chainlink-brownie-contracts/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 // Why is this a library and not abstract?
 // Why not an interface?
 library PriceConverter {
     // We could make this public, but then we'd have to deploy it
-    function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
+    function getPrice(
+        AggregatorV3Interface priceFeed
+    ) internal view returns (uint256) {
         // Sepolia ETH / USD Address
         // https://docs.chain.link/data-feeds/price-feeds/addresses
         (, int256 answer, , , ) = priceFeed.latestRoundData();
