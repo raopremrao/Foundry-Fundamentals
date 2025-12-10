c3aead89258090076092551886ab96f90e8513ca
raopremrao <raom61337@gmail.com>
2025-12-10 12:08:22 +0530

    Started Smart Contract Lottery

---
+++ Foundry-Smart-Contract-Lottery/src/Raffle.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+/**
+ * @title A Sample Raffle Contract
+ * @author T Prem
+ * @notice This Contract is for creating a sample raffle
+ * @dev Implements Chainlink VRFv2.5
+ */
+
+contract Raffle {
+    /* ERRORS */
+    error Raffle__SendMoreToEnterRaffle();
+
+    uint256 private immutable i_enteranceFee;
+
+    constructor(uint256 enteranceFee) {
+        i_enteranceFee = enteranceFee;
+    }
+
+    function enterRaffle() public payable {
+        // require(msg.value >= i_enteranceFee, "Not Enough ETH Send");
+        if (msg.value < i_enteranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+    }
+
+    function pickWinner() public {}
+
+    /** Getter Functions */
+
+    function getEnteranceFee() external view returns (uint256) {
+        return (i_enteranceFee);
+    }
+}
+
+// Style Guide:
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
